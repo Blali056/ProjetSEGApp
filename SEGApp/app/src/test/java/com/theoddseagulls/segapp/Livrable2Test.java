29c62b38c27e5d30296635c585bafea61beaf793
mounalaouane <mlaou091@uottawa.ca>
2018-11-10 15:05:17 -0500

    added testing code

--- SEGApp/app/src/test/java/com/theoddseagulls/segapp/Livrable2Test.java
+++ SEGApp/app/src/test/java/com/theoddseagulls/segapp/Livrable2Test.java
@@ -14,7 +14,28 @@ public class Livrable2Test {
         AdminAccount.getTheaccount("test@email.com", "secret");
         Account anAdminAccount = AdminAccount.getTheaccount();
         assertEquals("Check the email of the admin", "test@email.com",  anAdminAccount.getEmail());
-        //assertEquals("Check the pass of the admin", "secret",  anAdminAccount.getPassword());
+    }
+    @Test
+    public void checkAdminPass() {
+        AdminAccount.getTheaccount("test@email.com", "secret");
+        AdminAccount anAdminAccount = AdminAccount.getTheaccount();
+        assertEquals("Check the email of the admin", "secret",  anAdminAccount.getPassword());
+    }
+    @Test
+    public void checkProviderAccount() {
+        ProviderAccount providerAcc = new ProviderAccount("pro@email.com", "secret");
+        assertEquals("Check the email of a provider", "pro@email.com",  providerAcc.getEmail());
+    }
+    @Test
+    public void checkServiceGetService() {
+        Service service = new Service("Painting Job", 15);
+        assertEquals("Check the name of the service", "Painting Job",  service.getService());
+    }
+    @Test
+    public void checkServiceSetId() {
+        Service service = new Service("Painting Job", 15);
+        service.setId(1);
+        assertSame("Check the price of the service", 1, service.getId());
     }
 
 
