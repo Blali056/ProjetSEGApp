902bdd1c7d2eb7a5b5c72a1fadcd68ac3359ea50
labal085 <labal085@uottawa.ca>
2018-11-16 12:43:58 -0500

    addProviderService

---
+++ SEGApp/app/src/main/java/com/theoddseagulls/segapp/AddProviderService.java
@@ -0,0 +1,49 @@
+package com.theoddseagulls.segapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddProviderService extends AppCompatActivity {
+    private EditText serviceName;
+    private static  DB_handler mydatabase;
+    private ProviderAccount provider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addservice);
+
+        serviceName = (EditText) findViewById(R.id.serviceNameAdded);
+        mydatabase = new DB_handler(this);
+        String providerEmail = getIntent().getStringExtra("EMAIL");
+        provider = mydatabase.findProviderAccount(providerEmail);
+    }
+    public boolean serviceExist(){
+        boolean exist = true;
+        Service serviceInList = mydatabase.findService(serviceName.getText().toString());
+        if(serviceInList == null)
+            exist = false;
+        return exist;
+    }
+    public void addClick(View view){
+        if( serviceName.getText().length() == 0 ){
+            serviceName.setError("Entrez un service" );
+        }
+        else if(serviceExist() == true ){       // Si le service existe deja
+            serviceName.setError("Service existant");
+        }
+        else{
+            String service = serviceName.getText().toString();;
+            mydatabase.addProviderService(provider.getUsername(),service);
+            Context context = getApplicationContext();
+            Toast.makeText(context, "Service ajouté",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
--- SEGApp/app/src/main/java/com/theoddseagulls/segapp/EditProviderProfil.java
+++ SEGApp/app/src/main/java/com/theoddseagulls/segapp/EditProviderProfil.java
@@ -3,6 +3,9 @@ package com.theoddseagulls.segapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class EditProviderProfil extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class EditProviderProfil extends AppCompatActivity {
     private TextView postalCode;
     private TextView city;
     private TextView province;
+    private String userEmail;
 
 
     @Override
@@ -31,6 +35,23 @@ public class EditProviderProfil extends AppCompatActivity {
 
         // Ecris l'email du account
         email.setText( getIntent().getStringExtra("EMAIL") );
+        final String userEmail = getIntent().getStringExtra("EMAIL");
+
+
+
+
+        Button btn = (Button) findViewById(R.id.addService);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                    Intent intent = new Intent(getApplicationContext(), AddProviderService.class);
+
+                    intent.putExtra("EMAIL", userEmail);
+
+                    startActivityForResult(intent, 0);
+
+            }
+        });
 
     }
+
 }
